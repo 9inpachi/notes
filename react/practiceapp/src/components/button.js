698f134e12adee75f6ee3609f3d1ed793a19b177
ginpachi <m.fawaadali98@gmail.com>
2019-11-19 22:52:09 +0500

    Notes - OpenCL

--- react/practiceapp/src/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button({ title, href }) {
-    return (
-        <a class="btn btn-default" href={href} title={title}>{title}</a>
-    );
-}
-
-export default Button;
