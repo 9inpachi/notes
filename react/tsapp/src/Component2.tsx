e901c9ac62ebd6d52887d4155114387853c3b487
Fawad Ali <m.fawaadali98@gmail.com>
2020-10-26 23:27:25 +0500

    React - Minor code improvement

--- react/tsapp/src/Component2.tsx
+++ react/tsapp/src/Component2.tsx
@@ -6,9 +6,9 @@ const Component2: React.FC = () => {
 
   return (
     <div style={{ background: '#f0f0f0', flexGrow: 1, flexBasis: 0 }}>
-      <h1>This is component 2. Getting the username {value?.username}</h1>
+      <h1>This is component 2. Getting the username {value?.username}.</h1>
     </div>
-  )
+  );
 };
 
 export default Component2;
