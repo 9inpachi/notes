fced64d26a26a17dcffc01e357e93b066d9615bc
Fawad Ali <m.fawaadali98@gmail.com>
2021-05-05 23:09:39 +0500

    DSA - Circular queue implementation

---
+++ dsa/DSA/src/CircularQueue.java
@@ -0,0 +1,69 @@
+/**
+ * FIFO - First In First Out.
+ */
+public class CircularQueue {
+  private int[] array;
+  private int front;
+  private int rear;
+  private int size;
+
+  CircularQueue(int queueSize) {
+    array = new int[queueSize];
+    size = queueSize;
+    front = -1;
+    rear = -1;
+  }
+
+  public CircularQueue enqueue(int value) {
+    if (isFull()) {
+      System.out.println("CircularQueue is full.");
+    } else {
+      if (front == -1) {
+        front = 0;
+      }
+      rear = (rear + 1) % size;
+      array[rear] = value;
+    }
+
+    return this;
+  }
+
+  public CircularQueue dequeue() {
+    if (isEmpty()) {
+      System.out.println("CircularQueue is empty.");
+    } else {
+      if (front == rear) {
+        front = -1;
+        rear = -1;
+      } else {
+        front = (front + 1) % size;
+      }
+    }
+
+    return this;
+  }
+
+  public Boolean isEmpty() {
+    return front == -1;
+  }
+
+  public Boolean isFull() {
+    return (front == 0 && rear == size - 1) || (front == rear + 1);
+  }
+
+  public void print() {
+    int i;
+    for (i = front; i != rear; i = (i + 1) % size) {
+      System.out.println("i=" + i + " - value=" + array[i] + " - front=" + front + " - rear=" + rear);
+      // System.out.print(array[i] + (i == rear - 1 ? "" : "__"));
+    }
+    System.out.println("i=" + i + " - value=" + array[i] + " - front=" + front + " - rear=" + rear);
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    CircularQueue queue = new CircularQueue(4);
+    queue.enqueue(1).enqueue(2).enqueue(3).enqueue(4).dequeue().enqueue(5).dequeue().dequeue().enqueue(6);
+    queue.print();
+  }
+}
