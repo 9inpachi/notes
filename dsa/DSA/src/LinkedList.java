2fd7cab215951930b29d8ab87bc526af3ec8a6f6
Fawad Ali <m.fawaadali98@gmail.com>
2021-05-13 01:34:55 +0500

    DSA - Extend linked list

--- dsa/DSA/src/LinkedList.java
+++ dsa/DSA/src/LinkedList.java
@@ -11,52 +11,107 @@ class Node {
 public class LinkedList {
   Node head = null;
 
-  LinkedList insert(int value) {
+  LinkedList insertEnd(int value) {
     if (head == null) {
       head = new Node(value);
       return this;
     }
 
-    Node node = head;
+    Node temp = head;
 
-    while (node.next != null) {
-      node = node.next;
+    while (temp.next != null) {
+      temp = temp.next;
     }
 
     Node newNode = new Node(value);
-    node.next = newNode;
+    temp.next = newNode;
 
     return this;
   }
 
-  LinkedList delete(int value) {
-    Node node = head;
-    Node prevNode = node;
+  LinkedList insertStart(int value) {
+    Node newNode = new Node(value);
+
+    newNode.next = head;
+    head = newNode;
+
+    return this;
+  }
+
+  LinkedList insertMiddle(int value) {
+    if (head == null) {
+      head = new Node(value);
+      return this;
+    }
+
+    Node temp = head;
+    while (temp.next != null) {
+      if (temp.data <= value && temp.next.data >= value) {
+        break;
+      }
+      temp = temp.next;
+    }
+
+    Node newNode = new Node(value);
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    return this;
+  }
+
+  LinkedList deleteByValue(int value) {
+    Node temp = head;
+    Node prevNode = temp;
     Boolean nodeFound = false;
 
-    while (node.next != null) {
-      if (node.data == value) {
+    while (temp != null) {
+      if (temp.data == value) {
         nodeFound = true;
         break;
       }
-      prevNode = node;
-      node = node.next;
+      prevNode = temp;
+      temp = temp.next;
     }
 
     if (nodeFound) {
-      prevNode.next = node.next;
-      node = null;
+      prevNode.next = temp.next;
+      temp = null;
     }
+
+    return this;
+  }
+
+  LinkedList deleteStart() {
+    Node temp = head;
+    head = temp.next;
+    temp = null;
+
+    return this;
+  }
+
+  LinkedList deleteEnd() {
+    if (head.next == null) {
+      head = null;
+      return this;
+    }
+
+    Node temp = head;
+    Node prevNode = temp;
+    while (temp.next != null) {
+      prevNode = temp;
+      temp = temp.next;
+    }
+    prevNode.next = null;
 
     return this;
   }
 
   void print() {
-    Node node = head;
+    Node temp = head;
 
-    while (node != null) {
-      System.out.print(node.data + "__");
-      node = node.next;
+    while (temp != null) {
+      System.out.print(temp.data + "__");
+      temp = temp.next;
     }
 
     System.out.println();
@@ -65,9 +120,10 @@ public class LinkedList {
   public static void main(String[] args) {
     LinkedList linkedList = new LinkedList();
 
-    linkedList.insert(1).insert(2).insert(3).insert(4).insert(5);
+    linkedList.insertEnd(1).insertEnd(2).insertEnd(4).insertEnd(4).insertEnd(5);
     linkedList.print();
-    linkedList.delete(2).delete(3).delete(10);
+    linkedList.deleteByValue(2).deleteByValue(3).deleteByValue(5).deleteByValue(15);
+    linkedList.deleteEnd().deleteEnd().deleteEnd();
     linkedList.print();
   }
 }
