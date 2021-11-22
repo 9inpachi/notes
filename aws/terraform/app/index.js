9d01e480d72d29af36338fe389f32646b60cc2d3
Fawad Ali <m.fawaadali98@gmail.com>
2021-11-22 14:38:51 +0100

    Terraform - add sample app for terraform

---
+++ aws/terraform/app/index.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+
+app.get("/", (_req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, "localhost", () => {
+  console.log("Server running at http://localhost:3000");
+});
