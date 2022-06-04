6060d179b08d03b272939cb76a0b6c8f7e896f6d
Fawad Ali <m.fawaadali98@gmail.com>
2022-06-04 10:32:04 +0200

    Kotlin - Implement custom range

--- kotlin/conventions.kts
+++ kotlin/conventions.kts
@@ -1,3 +1,5 @@
+import java.util.Calendar
+
 // 1. Comparison and Operator Overloading
 
 data class Date(val year: Int, val month: Int, val day: Int) : Comparable<Date> {
@@ -6,10 +8,6 @@ data class Date(val year: Int, val month: Int, val day: Int) : Comparable<Date> 
       month != other.month -> month - other.month
       else -> day - other.day
     }
-  
-  override operator fun rangeTo(other: Date) Collection<Date> {
-    
-  }
 }
 
 val date1 = Date(2012, 12, 5)
@@ -17,3 +15,45 @@ val date2 = Date(2022, 12, 5)
 
 println(date1 < date2)
 // println(Date(2010, 12, 3) in date1,,date2)
+
+// 2. For Loop and Custom Range
+
+fun Date.followingDate(): Date {
+  val c = Calendar.getInstance()
+  c.set(this.year, this.month, this.day)
+  val dayTimeInMs = 24 * 60 * 60 * 1000L
+  val timeInMs = c.timeInMillis + dayTimeInMs
+  val result = Calendar.getInstance()
+  result.timeInMillis = timeInMs
+
+  return Date(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+class DateRange(val start: Date, val end: Date): Iterable<Date> {
+  override fun iterator(): Iterator<Date> = object : Iterator<Date> {
+    var current = start
+
+    override fun next(): Date {
+      if (!this.hasNext()) throw NoSuchElementException()
+      val prev = current
+      current = prev.followingDate()
+
+      return prev
+    }
+
+    override fun hasNext(): Boolean = current <= end
+  }
+}
+
+operator fun Date.rangeTo(other: Date) = DateRange(this, other)
+
+val startDate = Date(2022, 1, 1)
+val endDate = Date(2022, 1, 5)
+
+if (Date(2022, 1, 3) !in startDate..endDate) {
+  println("Date is there")
+}
+
+for (date in startDate..endDate) {
+  println(date)
+}
