6fad88f1d3c952b449c67c2db2c0c408b3d5fb30
Fawad Ali <m.fawaadali98@gmail.com>
2022-06-29 20:51:43 +0200

    Kotlin - More on collections

--- kotlin/collections.kts
+++ kotlin/collections.kts
@@ -24,3 +24,40 @@ val associateList = listOf("item 1", "item 2", "item 3")
 println(associateList.associateBy{ it.split(" ")[1] })
 println(associateList.associateWith{ it.split(" ")[1] })
 println(associateList.associate{ it.split(" ")[1] to it.split(" ")[0] })
+
+// 5. Group By
+
+val groupByList = listOf("a", "aa", "b", "bb")
+
+println(groupByList.groupBy{ it.length })
+
+// 6. Partition and Destructuring
+
+val partitionList = listOf("a", "aa", "b", "bb")
+
+val partition = partitionList.partition{ it.contains("a") }
+println(partition)
+
+val (positivePart, negativePart) = partition
+// Destructuring is done by `componentN` functions. `component1` gives the first element, `component2` the second and so on.
+println(positivePart == partition.component1())
+println(negativePart == partition.component2())
+
+// 7. FlatMap
+
+data class Product(val name: String, val priceTiers: List<Int>)
+
+// Flatten nested collections to a single list. In this case, `priceTiers` are flattened to a single list.
+val flatMapList = listOf(Product("a", listOf(1, 2, 3)), Product("b", listOf(3, 4, 5)))
+
+println(flatMapList.flatMap{ it.priceTiers })
+
+// 8. Min and Max
+
+val minMaxList = listOf(Product("a", listOf(1, 2)), Product("b", listOf(3, 4, 5)))
+val allPrices = minMaxList.flatMap(Product::priceTiers)
+
+println(allPrices.minOrNull())
+println(allPrices.maxOrNull())
+println(minMaxList.minByOrNull{ it.priceTiers.size })
+println(minMaxList.maxByOrNull{ it.priceTiers.size })
