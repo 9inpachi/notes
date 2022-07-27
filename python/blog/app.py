c7aaca6d47ae43bfed9ff66f501662333d2deee3
Fawad Ali <m.fawaadali98@gmail.com>
2022-07-28 04:28:39 +0500

    Python - Create post endpoint for app

--- python/blog/app.py
+++ python/blog/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-from db import get_db_connection
+from helpers import get_db_connection, get_post
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-  connection = get_db_connection()
-  posts = connection.execute("SELECT * FROM posts").fetchall()
-  return render_template("index.html", posts=posts)
+    connection = get_db_connection()
+    posts = connection.execute("SELECT * FROM posts").fetchall()
+    connection.close()
+
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    return render_template("post.html", post=get_post(post_id))
---
+++ python/blog/helpers.py
@@ -0,0 +1,23 @@
+import os
+import sqlite3
+from werkzeug.exceptions import abort
+
+db_file = os.path.join(os.path.dirname(__file__), "db/blogs.db")
+
+
+def get_db_connection():
+    connection = sqlite3.connect(db_file)
+    connection.row_factory = sqlite3.Row
+
+    return connection
+
+
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute(f"SELECT * FROM posts WHERE id = {post_id}").fetchone()
+    connection.close()
+
+    if post is None:
+        abort(404)
+
+    return post
