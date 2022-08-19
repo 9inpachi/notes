139fc8154b4b06ea5ed6b1dde5cca50eeb6bd078
Fawad Ali <m.fawaadali98@gmail.com>
2022-08-19 17:17:32 +0200

    Kotlin - Spring Boot - Make username column unique

--- kotlin/spring-boot/kotlin-auth-rest/src/main/resources/sql/schema.sql
+++ kotlin/spring-boot/kotlin-auth-rest/src/main/resources/sql/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id       varchar(60) DEFAULT RANDOM_UUID() PRIMARY KEY,
-    username varchar(10) NOT NULL,
+    username varchar(10) NOT NULL UNIQUE,
     password varchar     NOT NULL
 );
