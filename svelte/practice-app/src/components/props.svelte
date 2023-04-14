6231a5678a3ba65f662fc0deeeda6deed570b804
Fawad Ali <m.fawaadali98@gmail.com>
2023-04-14 18:02:33 +0200

    Svelte - Notes on props

---
+++ svelte/practice-app/src/components/props.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let someProp: number;
+</script>
+
+<p>This component is taking a prop someProp={someProp}</p>
--- svelte/practice-app/src/routes/+page.svelte
+++ svelte/practice-app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Reactivity from '../components/reactivity.svelte';
+	import PropsUser from '../components/props-user.svelte';
 </script>
 
 <Reactivity />
+<PropsUser />
